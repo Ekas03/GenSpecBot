1701dfdec9d8ba1c5b348be8bf56468bc28df8bc
Egor <69798266+Ekas03@users.noreply.github.com>
2025-06-17 10:12:42 +0300

    Update bot.py

--- GeneralSpecificBot/bot.py
+++ GeneralSpecificBot/bot.py
@@ -4,14 +4,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 
 from config import API_TOKEN
-#from db.session import init_db
 from handlers import register_all_handlers
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 dp = Dispatcher()
 
 async def main():
-    #await init_db()
     register_all_handlers(dp)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
